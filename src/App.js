2eeeb2533721a436860d60298ac0a6897aa74a52
Brian Douglas <bdougie@users.noreply.github.com>
2018-10-11 16:23:28 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React with Now
+            Learn React with Now!
           </a>
         </header>
       </div>
