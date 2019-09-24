013f39e945162d1ece758e6f2ad56d7f322cd889
Brian Douglas <bdougie@users.noreply.github.com>
2019-09-24 07:00:13 -0700

    Update run.rb

--- action/run.rb
+++ action/run.rb
@@ -2,16 +2,21 @@
 require 'octokit'
 
 NWO = ENV["GITHUB_REPOSITORY"]
+user = ENV["GITHUB ACTOR"]
 
 client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
-client.auto_paginate = true
+# client.auto_paginate = true
 
-open_issues = client.list_issues(NWO, { :labels => "available" })
+# open_issues = client.list_issues(NWO, { :labels => "available" })
 
-open_issues.each do |issue|
-  client.close_issue(NWO, issue.number)
+# open_issues.each do |issue|
+#   client.close_issue(NWO, issue.number)
+# end
+
+print "-------------------------------------"
+print client.organization_member?("github-craftwork", user)
+print "-------------------------------------"
+
+if !client.organization_member?("github-craftwork", user)
+  client.add_team_membership("github-craftwork", user)
 end
-
-print "-------------------------------------"
-print client.organization_member?("github-actor", "ENV["GITHUB ACTOR"])
-print "-------------------------------------"
