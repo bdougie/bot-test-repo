22a6b66264d3d0853df16644c622946ae6048f9d
Brian Douglas <bdougie@users.noreply.github.com>
2019-09-24 06:55:44 -0700

    Update run.rb

--- action/run.rb
+++ action/run.rb
@@ -2,15 +2,16 @@
 require 'octokit'
 
 NWO = ENV["GITHUB_REPOSITORY"]
-ISSUE_TEMPLATE = File.read(File.join(File.dirname(__FILE__), "template.md"))
 
 client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
 client.auto_paginate = true
 
-open_issues = client.list_issues(NWO, { :labels => "weekly-meeting" })
+open_issues = client.list_issues(NWO, { :labels => "available" })
 
 open_issues.each do |issue|
   client.close_issue(NWO, issue.number)
 end
 
-client.create_issue(NWO, "Weekly Meeting" , ISSUE_TEMPLATE, { :labels => "weekly-meeting" })
+print "-------------------------------------"
+print client.organization_member?(ENV["GITHUB ACTOR"])
+print "-------------------------------------"
