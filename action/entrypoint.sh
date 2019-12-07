10d0058258358b06a4be96b681c47c5116b9d4d1
Brian Douglas <bdougie@users.noreply.github.com>
2019-12-06 18:30:42 -0800

    Update entrypoint.sh

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # shellcheck disable=SC2046 disable=SC2048 disable=SC2086
 if ! git diff-index --quiet HEAD~$(jq '.commits | length' "${GITHUB_EVENT_PATH}") $*; then
-  echo $GITHUB_EVENT_PATH
+  echo $*
   echo "Changes in $*, proceeding"
 else
   echo "No changes in $*, stopping" && echo "ignore:$*" >> "$HOME/ignore"
