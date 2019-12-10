74a03e33ab3969024a8992b78e8a7da0759075d4
Brian Douglas <bdougie@users.noreply.github.com>
2019-12-10 13:39:55 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
+// If you want your app to work ofdsfdfdfline and load faster, you can change
 // unregister() to rsssdcsdsfsdssdfsdfegister() below. Note this comes with some pitfalls.
 // Learn more about service wvfdvorkers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
