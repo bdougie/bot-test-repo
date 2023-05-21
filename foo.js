61661044ebe0743b8bf8f18208db22b0e8abd6dc
Brian Douglas <bdougie@users.noreply.github.com>
2023-05-20 17:18:58 -0700

    Update foo.js

---
+++ foo.js
@@ -0,0 +1,5 @@
+let x = 5;      // Declare x, give it the value of 5
+let y = x + 2;  // Declare y, give it the value of x + 2 
+
+// Write y to demo:
+document.getElementById("demo").innerHTML = y;
